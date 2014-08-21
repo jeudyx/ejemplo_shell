3ad9bf54f911aa873e8da5ce8aa2033c8c89b9c3
estudiante <estudiante@fs0518.(none)>
2014-08-20 23:52:15 -0600

    Agrega archivo

---
+++ simple_script.sh
@@ -0,0 +1,9 @@
+#!/bin/sh 
+# Esto es un comentario
+echo "Cantidad de parametros pasados: $#" 
+echo "Los parametros pasados son: $*" 
+echo "El primero es: $1" 
+echo "Digite un valor: " 
+read number 
+echo "El valor digitado es: $number"
+
