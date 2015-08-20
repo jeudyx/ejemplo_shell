3b281b67e0ed4c361dd2ef1b670d97863ab83b7e
Estudiante <estudiante@ucr.ac.cr>
2015-08-20 17:20:39 -0600

    Un atajo

--- simple_script.sh
+++ simple_script.sh
@@ -4,7 +4,4 @@ echo "Cantidad de parametros pasados: $#"
 echo "Los parametros pasados son: $*" 
 echo "El primero es: $1" 
 echo "Digite un valor: " 
-read number 
-echo "El valor digitado es: $number"
 
-asasasasa
